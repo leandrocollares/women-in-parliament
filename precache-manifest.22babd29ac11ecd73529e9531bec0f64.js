9db3e1f1ee7fd7e5482dce1f088bec2d70298789
Leandro Collares <leco@uvic.ca>
2019-06-29 15:25:55 -0300

    Updates

---
+++ precache-manifest.22babd29ac11ecd73529e9531bec0f64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbfdd0a25a8f1823cfa4",
+    "url": "/women-in-parliament/static/js/runtime~main.4e3ffd52.js"
+  },
+  {
+    "revision": "4d9a5e6719945a798a64",
+    "url": "/women-in-parliament/static/js/main.00ca6986.chunk.js"
+  },
+  {
+    "revision": "09395f11bdc2a79d0d17",
+    "url": "/women-in-parliament/static/js/2.20afd788.chunk.js"
+  },
+  {
+    "revision": "4d9a5e6719945a798a64",
+    "url": "/women-in-parliament/static/css/main.c4289d58.chunk.css"
+  },
+  {
+    "revision": "683177554630d46d21009a302c1a9b92",
+    "url": "/women-in-parliament/index.html"
+  }
+];
